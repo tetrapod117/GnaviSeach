8056fd0a367d1c8256517698cd984c33ffb03bb5
tetrapod117 <tetrapod117@gmail.com>
2017-01-24 13:08:25 +0900

    最初のコミット

--- GnaviSeach/ViewController.swift
+++ GnaviSeach/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getJSON()
     }
-
+    //JSONを取得するメソッド
+    func getJSON(){
+        //APIのURLを定義する
+        let url = NSURL(string: "http://geoapi.heartrails.com/api/json?method=getAreas")!
+          //URLSessionで通信を行う
+        let task = URLSession.shared.dataTask(with: url as URL) { (data, response, error) in
+          //dataの中に値が入っているかのチェック
+            if let urlContent = data {
+                do{
+                    //NSDictionary形式にキャスト
+                    let jsonResult:NSDictionary = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                    
+                    if jsonResult.count > 0{
+                        //responseを取り出し、それもNSDictionaryにキャスト
+                        if let response = jsonResult["response"] as? NSDictionary {
+                            //responseからareaを取り出しNSArryにキャスト
+                            if let areas = response["area"] as? NSArray{
+                                //areaの数だけループさせ地域名を取り出し出力させる
+                                for area in areas {
+                                    if let name = area as? String {
+                                        print(name)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }catch{
+                    print("JSON serialization failed")
+                }
+            }
+            
+            
+        }
+        task.resume()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
